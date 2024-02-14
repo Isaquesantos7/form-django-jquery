df8637d98629a0b7c5e5beeec5cb127209dc2ce5
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-02-14 11:22:27 -0300

    listando ufs

--- federacao/views/formulario.py
+++ federacao/views/formulario.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from federacao.models import UF
 
 def formulario(request):
+    # lista de unidades federativas
+    lista_ufs = UF.objects.order_by('sigla').all()
+
+    context = {
+        'lista_ufs': lista_ufs
+    }
     
-    return render(request, 'base.html', context={})
+    return render(request, 'base.html', context)
